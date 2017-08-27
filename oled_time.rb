24fe112b8c163b10a44f2a63bdc115467e491768
glurp <regis.aubarede@gmail.com>
2017-08-27 12:16:15 +0200

    oled example

--- oled_time.rb
+++ oled_time.rb
@@ -7,7 +7,7 @@ def getMem()
  `free`.each_line {|line|
     next unless line=~ /^Mem:/
     mem=line.chomp.split(/\s+/)[3].to_i/1024
-    return("FreeMem: #{mem} MB")
+    return("FreeMem:   #{mem} MB")
  }
  "?"
 end
@@ -24,9 +24,15 @@ end
 oled=OnionOled.new(0,0)
 oled.reset
 oled.pos(0,0,ARGV.join(' ')) 
+nb=0
 loop {
    oled.pos(10,5,Time.now.to_s.split(' ')[1])
-   oled.pos(0,0,getMem())
-   oled.pos(0,1,getFlash())
+   if nb==0 || nb<60
+     oled.pos(0,0,getMem())
+     oled.pos(0,1,getFlash())
+     nb=0
+   else
+     nb+=1
+   end
    sleep 1
 }
